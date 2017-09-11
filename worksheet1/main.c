f7eac623b5b093fdebbffaa5d126f507284e2f7c
John Foley <jfoley@cs.umass.edu>
2017-09-11 11:43:28 -0400

    add first worksheet

---
+++ worksheet1/main.c
@@ -0,0 +1,33 @@
+#include <assert.h>
+
+int main(int argc, char* argv[]) {
+  assert(4 == addition(2,2));
+  assert(14 == addition(12,2));
+  assert(4 == addition(6,-2));
+  assert(10 == addition(-5,15));
+  assert(4 == addition(1,3));
+  assert(4 == addition(3,1));
+  
+  assert(0 == subtraction(2,2));
+  assert(10 == subtraction(12,2));
+  assert(8 == subtraction(6,-2));
+  assert(-20 == subtraction(-5,15));
+  assert(-2 == subtraction(1,3));
+  assert(2 == subtraction(3,1));
+  
+  assert(4 == multiplication(2,2));
+  assert(24 == multiplication(12,2));
+  assert(-12 == multiplication(6,-2));
+  assert(-75 == multiplication(-5,15));
+  assert(3 == multiplication(1,3));
+  assert(3 == multiplication(3,1));
+  
+  assert(1 == division(2,2));
+  assert(6 == division(12,2));
+  assert(-3 == division(6,-2));
+  assert(0 == division(-5,15));
+  assert(0 == division(1,3));
+  assert(3 == division(3,1));
+
+  return 0;
+}
---
+++ worksheet1/operations.c
@@ -0,0 +1,19 @@
+//the following two lines must not be changed, and will be explained in future weeks
+#include <assert.h>
+#include <stdio.h>
+
+//This function represents addition
+int addition(int op1, int op2){
+  // you must fill in this function so that it adds the op1 and op2 and returns the result
+}
+
+// Create a function for subtraction that is named "subtraction", takes two ints, and returns the difference. (You should subtract the SECOND number from the FIRST)
+
+// Create a function for multiplication that is named "multiplication", takes two ints, and returns the product.
+
+// Create a function for division that is named "division", takes two ints, and returns the quotient. (You should divide the first number by the second number. Keep in mind we are using integer division, so all answers will be automatically rounded down.)
+
+//NOTE: you cannot test your code until you have created the subtraction, multiplication, and division functions (though they do not need to work).
+
+// This tells the C compiler to copy and paste the testing code from main.c into this file. This is considered bad style, usually. We will discuss how to better compile code with multiple files next week.
+#include "main.c"
